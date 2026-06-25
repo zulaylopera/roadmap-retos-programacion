53955e09b387f41a70c2d95afe2a6d540628d746
zulaylopera <zulaylopeera@gmail.com>
2026-06-25 09:08:07 -0500

    Update zulaylopera.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/zulaylopera.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/zulaylopera.py
@@ -1,1 +1,22 @@
+# https://www.python.org/
+# comentario en una linea
+"""
+Esto también es un comentario
+en 
+varias lineas
+"""
 
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANTE = "Mi contaste"
+my_int = 1
+my_float: 1.5
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+
+
+Print("¡Hola, Python!")
+  
